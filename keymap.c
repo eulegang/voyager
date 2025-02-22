00bd63d3cf310902fdd408382ff36b1c9b901fee
eulegang <eulegang@eulegang.dev>
2025-02-22 11:17:39 -0600

    fixing equal key

--- keymap.c
+++ keymap.c
@@ -57,7 +57,7 @@ enum TD_KEYS {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LT_BASE] = LAYOUT_voyager(
-    US_MOD,         KC_1,           KC_2,           KC_3,           KC_4,           KC_5,                                           KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,
+    KC_EQUAL,       KC_1,           KC_2,           KC_3,           KC_4,           KC_5,                                           KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,
     KC_TAB,         KC_Q,           KC_W,           MO_E,           MO_R,           KC_T,                                           KC_Y,           MO_U,           MO_I,           KC_O,           KC_P,           KC_BSLS,
     MO_ESC,         MO_A,           MO_S,           MO_D,           MO_F,           MO_G,                                           MO_H,           MO_J,           MO_K,           MO_L,           MO_SCLN,        MO_QUOTE,
     KC_LSFT,        KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_RSFT,
